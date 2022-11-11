e3e791720f105985996106870d7ee518474d987f
Mariegolepas <97367355+Mariegolepas@users.noreply.github.com>
2022-11-11 21:56:45 +0100

    add product.js

--- front/js/product.js
+++ front/js/product.js
@@ -16,7 +16,31 @@ async function showDetailProduct() {
     console.log("l'ID récupéré via l'URL est :", id);
     let product = await getDetailsProduct(id);
     console.log("le produit récupéré est :", product);
+    
     //Ajout des éléments dans le DOM
+    let itemImg = document.querySelector('.item__img');
+    const imageKanap = document.createElement('img');
+    imageKanap.setAttribute('src', product.imageURL);
+    imageKanap.setAttribute('alt', product.altTxt);
+    itemImg.appendChild(imageKanap);
+
+    const pageName = document.querySelector('title');
+    pageName.textContent = product.name;
+
+    const productName = document.querySelector('#title');
+    productName.textContent = product.name;
+
+    const priceSpan = document.querySelector('#price');
+    priceSpan.textContent = product.price;
+
+    const productDescription = document.querySelector('#description');
+    productDescription.textContent = product.description;
+
+    const productColor = document.querySelector('option');
+    productColor.textContent = product.colors;
+
+
+
 
 }
 
